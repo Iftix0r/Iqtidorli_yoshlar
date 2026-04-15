22f841511e1bfd1b3bd7ea8286cc51ce213a4959
iftix0r <iftixortoychiyev1@gmail.com>
2026-04-15 19:03:00 +0500

    fix passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,8 @@
 import sys
 import os
 
-# Virtual environment Python interpreter
-INTERP = os.path.expanduser('~/virtualenv/yoshiqtidorlar/3.11/bin/python3')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+sys.path.insert(0, '/home/host7905/yoshiqtidorlar')
 
-# Loyiha root papkasini path ga qo'shish
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'iqtidorli_yoshlar.settings'
 
 from django.core.wsgi import get_wsgi_application
